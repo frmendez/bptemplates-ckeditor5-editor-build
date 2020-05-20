f7ab7e55fdaae8dca780a2397926f5265f0f26f4
Francisco Mendez <francisco.mendez@bleprintsit.cl>
2020-05-20 15:43:50 -0400

    Initial commit

---
+++ src/ckeditor.js
@@ -0,0 +1,191 @@
+/**
+ * @license Copyright (c) 2014-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import DecoupledDocumentEditor from '@ckeditor/ckeditor5-editor-decoupled/src/decouplededitor.js';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
+import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave.js';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder.js';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js';
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
+import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily.js';
+import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight.js';
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js';
+import Image from '@ckeditor/ckeditor5-image/src/image.js';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize.js';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import List from '@ckeditor/ckeditor5-list/src/list.js';
+import MathType from '@wiris/mathtype-ckeditor5';
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
+import PageBreak from '@ckeditor/ckeditor5-page-break/src/pagebreak.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript.js';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript.js';
+import Table from '@ckeditor/ckeditor5-table/src/table.js';
+import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperties';
+import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
+import TodoList from '@ckeditor/ckeditor5-list/src/todolist';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
+
+export default class Editor extends DecoupledDocumentEditor {}
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+	Alignment,
+	Autoformat,
+	Autosave,
+	BlockQuote,
+	Bold,
+	CKFinder,
+	Code,
+	Essentials,
+	FontBackgroundColor,
+	FontColor,
+	FontFamily,
+	FontSize,
+	Heading,
+	Highlight,
+	HorizontalLine,
+	Image,
+	ImageCaption,
+	ImageResize,
+	ImageStyle,
+	ImageToolbar,
+	ImageUpload,
+	Indent,
+	IndentBlock,
+	Italic,
+	Link,
+	List,
+	MathType,
+	MediaEmbed,
+	PageBreak,
+	Paragraph,
+	PasteFromOffice,
+	RemoveFormat,
+	Strikethrough,
+	Subscript,
+	Superscript,
+	Table,
+	TableCellProperties,
+	TableProperties,
+	TableToolbar,
+	TextTransformation,
+	TodoList,
+	Underline
+];
+
+
+const customColorPalette = [
+	{ color: '#000000', label: 'Black' },
+	{ color: '#4d4d4d', label: 'Dim grey' },
+	{ color: '#999999', label: 'Grey' },
+	{ color: '#e6e6e6', label: 'Light grey' },
+	{ color: '#ffffff', label: 'White', hasBorder: !0 },
+	{ color: '#e64c4c', label: 'Red' },
+	{ color: '#e6994c', label: 'Orange' },
+	{ color: '#e6e64c', label: 'Yellow' },
+	{ color: '#99e64c', label: 'Light green' },
+	{ color: '#4ce64c', label: 'Green' },
+	{ color: '#4ce699', label: 'Aquamarine' },
+	{ color: '#4ce6e6', label: 'Turquoise' },
+	{ color: '#4c99e6', label: 'Light blue' },
+	{ color: '#4c4ce6', label: 'Blue' },
+	{ color: '#994ce6', label: 'Purple' }
+];
+
+// Editor configuration.
+Editor.defaultConfig = {
+	toolbar: {
+		items: [
+			'heading',
+			'|',
+			'fontfamily',
+			'fontsize',
+			'fontColor',
+			'fontBackgroundColor',
+			'|',
+			'bold',
+			'italic',
+			'underline',
+			'strikethrough',
+			'|',
+			'alignment',
+			'|',
+			'numberedList',
+			'bulletedList',
+			'|',
+			'indent',
+			'outdent',
+			'|',
+			'MathType',
+			'ChemType',
+			'|',
+			'blockquote',
+			'imageUpload',
+			'insertTable',
+			'|',
+			'undo',
+			'redo'
+		]
+	},
+	image: {
+		styles: [
+			'full',
+			'alignLeft',
+			'alignRight'
+		],
+		toolbar: [
+			'imageStyle:alignLeft',
+			'imageStyle:full',
+			'imageStyle:alignRight',
+			'|',
+			'imageTextAlternative'
+		]
+	},
+	table: {
+		contentToolbar: [
+			'tableColumn', 'tableRow', 'mergeTableCells',
+			'tableProperties', 'tableCellProperties'
+		],
+		tableProperties: {
+			borderColors: customColorPalette,
+			backgroundColors: customColorPalette
+		},
+
+		// Set the palettes for table cells.
+		tableCellProperties: {
+			borderColors: customColorPalette,
+			backgroundColors: customColorPalette
+		}
+	},
+	// This value must be kept in sync with the language defined in webpack.config.js.
+	language: 'es',
+
+	fontBackgroundColor: {
+		colors: customColorPalette
+	},
+	fontColor: {
+		colors: customColorPalette
+	}
+
+};
